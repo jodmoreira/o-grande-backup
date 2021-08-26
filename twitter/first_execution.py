ecdfc0aee15efecc04501c99115c3a7bf170302b
Jod Moreira <jodilsonmoreira@gmail.com>
2021-08-26 21:01:18 +0300

    Twitter streaming api and s3 integration

---
+++ twitter/first_execution.py
@@ -0,0 +1,12 @@
+import main_twitter_ogb
+import database
+
+
+
+
+# screen_name = 'TerezaCrisMS'
+# user_id = main_twitter_ogb.get_user_id('TerezaCrisMS')
+# data = (screen_name, user_id)
+# database.add_new_profile(data)
+print(database.read_profiles())
+
