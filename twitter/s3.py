46aa78ab035af4341b81ba08f18edf165daf2781
Ubuntu <ubuntu@ip-172-31-43-104.us-east-2.compute.internal>
2021-08-26 19:09:50 +0000

    removes blank space from screen name

--- twitter/s3.py
+++ twitter/s3.py
@@ -4,9 +4,9 @@ import json
 s3 = boto3.client('s3')
 
 def upload(content):
+    screen_name = str(content["user"]["screen_name"])
+    screen_name = screen_name.replace(' ','')
     s3.put_object(Body=(bytes(json.dumps(content).encode('UTF-8'))),
     Bucket='ogb-dados-gerais',
-    screen_name = content["user"]["screen_name"]
-    screen_name = screen_name.replace(' ','')
     Key=f'{screen_name}/{screen_name}_{content["id_str"]}.json',
     StorageClass='STANDARD_IA')
