dd6f344da5a58026f8148ce0354714fad09cf75f
jodmoreira <31655505+jodmoreira@users.noreply.github.com>
2021-09-06 05:13:55 -0300

    wrog dir

--- o-grande-backup/branches/master/twitter/s3.py
+++
@@ -1,9 +0,0 @@
-import boto3
-from datetime import datetime
-
-
-cli = boto3.client('s3')
-cli.put_object(
-  Body='The time now is '+str(datetime.now()),
-  Bucket='ogb-dados-gerais',
-  Key='ec2.txt')
