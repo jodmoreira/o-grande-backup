94a08df05231d7f48c0397ce058dae74cdcd72ec
Jod Moreira <jodmoreira@Jods-MacBook-Air.local>
2021-12-04 21:02:30 +0200

    webflow module to deal with api

---
+++ wf/main.py
@@ -0,0 +1,107 @@
+import os
+import requests
+import json
+import time
+
+API_KEY = os.environ.get("WFAPI")
+MAIN_URL = "https://api.webflow.com"
+
+
+def authorization_info():
+    url = f"{MAIN_URL}/info"
+    payload = {}
+    headers = {"Authorization": f"Bearer {API_KEY}", "accept-version": "1.0.0"}
+    time.sleep(1)
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    return response.text
+
+
+def list_sites():
+    url = f"{MAIN_URL}/sites"
+    payload = {}
+    headers = {"Authorization": f"Bearer {API_KEY}", "accept-version": "1.0.0"}
+    time.sleep(1)
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    return response.text
+
+
+def get_specific_site(site_id):
+    url = f"{MAIN_URL}/sites/{site_id}"
+    payload = {}
+    headers = {"Authorization": f"Bearer {API_KEY}", "accept-version": "1.0.0"}
+    time.sleep(1)
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    return response.text
+
+
+def list_collections(site_id):
+    url = f"{MAIN_URL}/sites/{site_id}/collections"
+    payload = {}
+    headers = {"Authorization": f"Bearer {API_KEY}", "accept-version": "1.0.0"}
+    time.sleep(1)
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    return response.text
+
+
+def get_collections(site_id, collection_id):
+    url = f"{MAIN_URL}/sites/{site_id}/collections/{collection_id}"
+    payload = {}
+    headers = {"Authorization": f"Bearer {API_KEY}", "accept-version": "1.0.0"}
+    time.sleep(1)
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    return response.text
+
+
+def get_all_items_from_collections(site_id, collection_id):
+    url = f"{MAIN_URL}/sites/{site_id}/collections/{collection_id}/items"
+    payload = {}
+    headers = {"Authorization": f"Bearer {API_KEY}", "accept-version": "1.0.0"}
+    time.sleep(1)
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    return response.text
+
+
+def get_single_item(site_id, collection_id, item_id):
+    url = f"{MAIN_URL}/sites/{site_id}/collections/{collection_id}/items/{item_id}"
+    payload = {}
+    headers = {"Authorization": f"Bearer {API_KEY}", "accept-version": "1.0.0"}
+    time.sleep(1)
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    return response.text
+
+
+def create_collection_item(site_id, collection_id, data):
+    url = f"{MAIN_URL}/sites/{site_id}/collections/{collection_id}/items"
+    payload = json.dumps(data)
+    headers = {"Authorization": f"Bearer {API_KEY}", "accept-version": "1.0.0"}
+    time.sleep(1)
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    return response.text
+
+
+def update_collection_item(site_id, collection_id, item_id, data):
+    url = f"{MAIN_URL}/sites/{site_id}/collections/{collection_id}/items/{item_id}"
+    payload = json.dumps(data)
+    headers = {"Authorization": f"Bearer {API_KEY}", "accept-version": "1.0.0"}
+    time.sleep(1)
+    response = requests.request("PUT", url, headers=headers, data=payload)
+
+    return response.text
+
+
+def patch_collection_item(site_id, collection_id, item_id):
+    url = f"{MAIN_URL}/sites/{site_id}/collections/{collection_id}/items/{item_id}"
+    payload = json.dumps(data)
+    headers = {"Authorization": f"Bearer {API_KEY}", "accept-version": "1.0.0"}
+    time.sleep(1)
+    response = requests.request("PATCH", url, headers=headers, data=payload)
+
+    return response.text
