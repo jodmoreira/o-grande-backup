48d81525c07694afcb0078eb4d98dbaecd0f8870
Ubuntu <ubuntu@ip-172-31-43-104.us-east-2.compute.internal>
2021-12-06 18:58:24 +0000

    not yet finalized setup_too,s and s3_tools

--- aws_tools/s3_tools.py
+++ aws_tools/s3_tools.py
@@ -12,6 +12,23 @@ def list_files(bucket_name, prefix, max_keys):
     return keys
 
 
+def list_agents(social_media_name, bucket_name):
+    all_agents = []
+    prefix_social_media = f"social_media/{social_media_name}/"
+    years = list_files(bucket_name, prefix_social_media, 1000)
+    for year in years:
+        prefix_year = f"{prefix_social_media}/{year}/"
+        months = list_files(bucket_name, prefix_year, 1000)
+        for month in months:
+            prefix_month = f"{prefix_year}/{month}/"
+            days = list_files(bucket_name, prefix_month, 1000)
+            for day in days:
+                prefix_day = f"{prefix_month}/{day}/"
+                agents = list_files(bucket_name, prefix_day, 1000)
+                all_agents.append(agents)
+    return all_agents
+
+
 def upload_file(bucket_name, file_path, key):
     s3.upload_file(file_path, bucket_name, key)
 
--- twitter/main_twitter_ogb.py
+++ twitter/main_twitter_ogb.py
@@ -5,95 +5,110 @@ import twitter_credentials
 import time
 import json
 import s3
+import os
 
-auth = OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
+CONSUMER_KEY = os.environ.get("TWITTER_CONSUMER_KEY")
+CONSUMER_SECRET = os.environ.get("TWITTER_CONSUMER_SECRET")
+
+auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 api = API(auth)
 
 
 ##### For the first execution #####
 def looper(last_id, new_user):
-    tweets = api.user_timeline(id=new_user, tweet_mode='extended', count=200, max_id=last_id)
+    tweets = api.user_timeline(
+        id=new_user, tweet_mode="extended", count=200, max_id=last_id
+    )
     if len(tweets) < 1:
         return
     for tweet in tweets:
         id_tweet = tweet.id
-        print(f'{id_tweet} from {new_user}')
+        print(f"{id_tweet} from {new_user}")
         s3.uploader(tweet._json)
-        last_id = id_tweet-1
+        last_id = id_tweet - 1
     try:
-        print('Going to new item')
+        print("Going to new item")
         looper(last_id, new_user)
     except Exception as e:
         print(e)
-        print('sleep')
-        time.sleep(15*60)
+        print("sleep")
+        time.sleep(15 * 60)
         looper(last_id, new_user)
 
+
 def first_execution(new_user):
-    '''
+    """
     Runs the frist API loop and returns the first 200 tweets and
     then starts looping
-    '''
+    """
     try:
-        tweets = api.user_timeline(id=new_user, tweet_mode='extended', count=200)
+        tweets = api.user_timeline(id=new_user, tweet_mode="extended", count=200)
         for tweet in tweets:
             id_tweet = tweet.id
-            print(f'{id_tweet} from {new_user}')
+            print(f"{id_tweet} from {new_user}")
             s3.uploader(tweet._json)
-            last_id = id_tweet-1
+            last_id = id_tweet - 1
         # looper(last_id, new_user)
     except tweepy.TweepError:
-        print(f'Unable to get data because {new_user} is not available')
+        print(f"Unable to get data because {new_user} is not available")
         pass
+
+
 def get_user_id(screen_name):
     id = api.get_user(screen_name).id
     return id
+
+
 ##
 ##
 ## Legacy functions
 def writer(tweet, user):
     try:
-        diretorio_escrita = fr'{FILES_DIR}{user}'
-        doc_saida = open(diretorio_escrita,'a+', encoding='utf8')
+        diretorio_escrita = fr"{FILES_DIR}{user}"
+        doc_saida = open(diretorio_escrita, "a+", encoding="utf8")
     except FileNotFoundError:
-        diretorio_escrita = f'{FILES_DIR}{user}'
-        doc_saida = open(diretorio_escrita,'a+', encoding='utf8')
+        diretorio_escrita = f"{FILES_DIR}{user}"
+        doc_saida = open(diretorio_escrita, "a+", encoding="utf8")
     tweet = json.dumps(tweet._json, ensure_ascii=False)
     doc_saida.write(tweet)
-    doc_saida.write('\n')
+    doc_saida.write("\n")
     doc_saida.close()
+
 
 def get_greater_id(user):
     try:
-        doc = open(fr'{DIRETORIO_ARQUIVOS}\{user}', 'r', encoding='utf8')
+        doc = open(fr"{DIRETORIO_ARQUIVOS}\{user}", "r", encoding="utf8")
     except FileNotFoundError:
-        doc = open(fr'{DIRETORIO_ARQUIVOS}/{user}', 'r', encoding='utf8')
+        doc = open(fr"{DIRETORIO_ARQUIVOS}/{user}", "r", encoding="utf8")
     doc = doc.readlines()
     doc_json = [json.loads(i) for i in doc]
-    ids = [i['id'] for i in doc_json]
+    ids = [i["id"] for i in doc_json]
     maior_id = max(ids)
-    maior_id = maior_id+1
+    maior_id = maior_id + 1
     return maior_id
+
 
 def run_since_id(user):
     maior_id = get_greater_id(user)
-    tweets = api.user_timeline(id=user, tweet_mode='extended', count=200, since_id=maior_id)
+    tweets = api.user_timeline(
+        id=user, tweet_mode="extended", count=200, since_id=maior_id
+    )
     if len(tweets) < 1:
-        print(f'nothing found from {user}' )
+        print(f"nothing found from {user}")
         return
     for tweet in tweets:
         id_tweet = tweet.id
-        print(f'{id_tweet} from {user}')
+        print(f"{id_tweet} from {user}")
         writer(tweet, user)
     try:
         run_since_id(user)
-        print('New item')
+        print("New item")
     except Exception as e:
         print(e)
-        print('sleep')
-        time.sleep(15*60)
+        print("sleep")
+        time.sleep(15 * 60)
         run_since_id(user)
 
 
-if __name__ == '__main__':
-    first_execution('jodmoreira')
+if __name__ == "__main__":
+    first_execution("jodmoreira")
